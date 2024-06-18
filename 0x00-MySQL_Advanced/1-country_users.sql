69ce71e7e0404ad56b845869cf79d56acf2f24d2
wessam18 <wessamabdelnabi903@gmail.com>
2024-06-18 06:49:55 +0300

    .

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- SQL script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    name VARCHAR(255)
+    country ENUM(US, CO, TN) NOT NULL DEFAULT(US)
 );
